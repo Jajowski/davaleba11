077519a4550625454ad4945b9a8aae2de54d214f
Jajowski <tamojajanidze26@gmail.com>
2023-02-23 23:59:52 +0400

    dvlb

---
+++ natura.js
@@ -0,0 +1,8 @@
+let naturisanteba = () => {
+
+    document.getElementById("natura").src = "img/bulb2.png"
+}
+
+let naturischakroba = () => {
+    document.getElementById("natura").src = "img/bulb1.png"
+}
